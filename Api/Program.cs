9072345eec5244defa0b0cbb07d90c92d6103b4e
mibrammall <mibrammall.github@gmail.com>
2024-04-22 20:34:25 +0100

    Put DB connection string into config

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,9 +1,9 @@
 using Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var ordersDbConnectionString = builder.Configuration.GetConnectionString("OrdersDb");
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
