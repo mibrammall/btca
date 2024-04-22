850e4540e2bb4cbdd5dc5f2bee7edfc8e2e7d172
mibrammall <mibrammall.github@gmail.com>
2024-04-22 22:44:22 +0100

    Return total through API

--- Api/Services/OrderService.cs
+++ Api/Services/OrderService.cs
@@ -11,53 +11,28 @@ namespace Api.Services
 
         public async Task<IEnumerable<Models.Order>> GetOrdersForCompany(int CompanyId)
         {
-            var result = await _orderRepository.Orders.Include(x => x.OrderProducts).Where(o => o.CompanyId == CompanyId).ToListAsync();
+            var result = await _orderRepository
+                .Orders
+                .Include(x => x.OrderProducts)
+                .ThenInclude(x => x.Product)
+                .Include(x => x.Company)
+                .Where(o => o.CompanyId == CompanyId)
+                .ToListAsync();
 
             return result.Select(x => new Models.Order
             {
-                CompanyName = x.CompanyId.ToString(),
-                OrderProducts = x.OrderProducts.Select(x => new Models.OrderProduct()),
+                CompanyName = x.Company.Name,
+                Description = x.Description,
+                OrderProducts = x.OrderProducts.Select(x => new Models.OrderProduct
+                {
+                    Product = new Models.Product
+                    {
+                        Name = x.Product.Name,
+                    },
+                    Price = x.Price
+                }),
+                OrderTotal = x.OrderProducts.Select(x => x.Price).Sum()
             });
-
-            // //Get the order products
-            // var sql2 =
-            //     "SELECT op.price, op.order_id, op.product_id, op.quantity, p.name, p.price FROM orderproduct op INNER JOIN product p on op.product_id=p.product_id";
-
-            // var reader2 = database.ExecuteReader(sql2);
-
-            // var values2 = new List<OrderProduct>();
-
-            // while (reader2.Read())
-            // {
-            //     var record2 = (IDataRecord)reader2;
-
-            //     values2.Add(new OrderProduct()
-            //     {
-            //         OrderId = record2.GetInt32(1),
-            //         ProductId = record2.GetInt32(2),
-            //         Price = record2.GetDecimal(0),
-            //         Quantity = record2.GetInt32(3),
-            //         Product = new Product()
-            //         {
-            //             Name = record2.GetString(4),
-            //             Price = record2.GetDecimal(5)
-            //         }
-            //     });
-            // }
-
-            // reader2.Close();
-
-            // foreach (var order in values)
-            // {
-            //     foreach (var orderproduct in values2)
-            //     {
-            //         if (orderproduct.OrderId != order.OrderId)
-            //             continue;
-
-            //         order.OrderProducts.Add(orderproduct);
-            //         order.OrderTotal = order.OrderTotal + (orderproduct.Price * orderproduct.Quantity);
-            //     }
-            // }
         }
     }
 }
